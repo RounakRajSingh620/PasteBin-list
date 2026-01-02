3b3bc57ff97e25dad445bf4284bcbf4efdc73bac
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2026-01-02 12:47:17 +0530

    Updated index

--- pastebin-lite/api/index.js
+++ pastebin-lite/api/index.js
@@ -1,4 +1,3 @@
-const serverless = require("serverless-http");
 const app = require("../src/vercel-server");
 
-module.exports = serverless(app);
+module.exports = app;
--- pastebin-lite/src/vercel-server.js
+++ pastebin-lite/src/vercel-server.js
@@ -8,10 +8,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("src/views"));
 
+app.get("/", (req, res) => res.json({ ok: true }));
+
 app.use("/api/healthz", require("./routes/health"));
 app.use("/api/pastes", require("./routes/pastes"));
 app.use("/api/pastes", require("./routes/pasteFetch"));
 app.use("/p", require("./routes/pasteView"));
-
 
 module.exports = app;
